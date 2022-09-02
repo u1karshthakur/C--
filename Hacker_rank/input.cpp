019a38f084bea57915c465c512ad89cb85bb987d
Utkarsh Thakur <thakurutkarsh453@gmail.com>
2022-09-03 01:17:15 +0530

    practice

---
+++ Hacker_rank/input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main ( ){
+  int a ; long b ; char c ; float d ; double e ; 
+  cin >>a >>b >>c >>d >>e ;
+
+  cout << a <<endl;
+  cout << b << endl;
+  cout << c << endl;
+  cout << d << endl;
+  cout << e << endl;
+
+  return 0 ;
+}
